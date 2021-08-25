5e745829cfd1b32b829a56284df499cfc8ec4846
Arif Darmawan <arif.darmawan@riflab.com>
2021-08-25 10:22:48 +0700

    update

--- src/main_gui1.py
+++ src/main_gui1.py
@@ -1,14 +1,15 @@
+# python -m PyQt5.uic.pyuic -x mainwindow.ui -o mainwindow.py
 # python -m PyQt5.uic.pyuic -x widget_1Dmod.ui -o widget_1Dmod.py
 
-
+import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
 from widget_1Dmod import Ui_OneDmod
+from mainwindow import Ui_MainWindow
 
 if __name__ == "__main__":
-    import sys
     app = QtWidgets.QApplication(sys.argv)
-    OneDmod = QtWidgets.QWidget()
-    ui = Ui_OneDmod()
-    ui.setupUi(OneDmod)
-    OneDmod.show()
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
     sys.exit(app.exec_())
