9cfe1e303eb06080aca174ee05e9034f80218422
Arif Darmawan <arif.darmawan@riflab.com>
2021-08-27 08:50:01 +0700

    create an error notification for check integer number for Period per Decade, Maximum Period and Number of Decade

--- src/general_module.py
+++ src/general_module.py
@@ -6,7 +6,7 @@ def error_dialog(error_list):
     msg = QtWidgets.QMessageBox()
     msg.setIcon(QtWidgets.QMessageBox.Warning)
     msg.setWindowTitle("Error Message")
-    msg.setText("Don't leave blank")
+    msg.setText("Fix the following issue")
     for i in error_list:
         temp.append(i + error_list[i])
     msg.setDetailedText("\n".join(temp))
@@ -16,4 +16,11 @@ def error_dialog(error_list):
 def check_error(val, text, error_list):
     if val == "":
         error_list[text] = " is blank"
+    else:
+        if text == "Period per Decade" or text == "Maximum Period" or text == "Number of Decade":
+            try:
+                int(val)
+            except Exception:
+                error_list[text] = " must be an integer"
+
     return error_list
